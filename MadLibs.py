a5b2b47b0afcf9140e15e4aa9fa3c89c6f687a75
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-04-09 16:44:35 -0600

    Completed Project
    Finished all of the functions and added the export functionality.

--- MadLibs.py
+++ MadLibs.py
@@ -6,7 +6,6 @@
 
 # Importing pkgs
 import os
-import sys
 
 # Clears the screen
 def cls():
@@ -23,17 +22,84 @@ User_Input = ""
 # Tracks if the User has made a decision
 User_Confirm = False
 
+# String that holds the complete User Story
+User_Story = ""
+
 ########################################################### PROGRAM LOGIC ################################################################
 
-### Loads the Mad Libs file and prompts the User to enter the relevant information
+### Loads the 'Outline.txt' file into the program
 def Load_File():
+
+    # Importing global variables
+    global Is_Running
+    global Outline
+
+    # Loads the 'Outline.txt' file and closes the program if the file cannot be found by the program.
+    try:
+        Outline = open("Outline.txt","r")
+    except:
+        print("*ERROR* 'Outline.txt' file not found. *ERROR*")
+        print("Please ensure that 'Outline.txt' is in the same folder as the 'MadLibs.py' file.")
+        input("Press 'enter' to close the program.")
+        Is_Running = False
+
+### Prompts the User to enter information to customize the MadLibs
+def Prompt_User():
 
     # Importing global variables
     global User_Input
     global User_Confirm
+    global User_Story
+    global Outline
 
-    input("Press 'enter' to continue.")
+    Line_Content = Outline.readline() # Reads what is in each line of the 'Outline.txt' file
 
+    while Line_Content: # While there are still lines left to be read
+        while "[" in Line_Content: # Parses through each line of 'Outline.txt' and finds every instance of '['
+            Start = Line_Content.find("[") # Defining the borders of the text to be replaced
+            End = Line_Content.find("]")
+            Prompt = Line_Content[Start + 1:End] # Assinging the text to be replaced to a variable
+            User_Input = input(Prompt + ": ") # Getting the text that the User wants to use to replace the placeholder text
+            Line_Content = Line_Content.replace("[" + Prompt + "]", User_Input) # Replacing the placeholder with the User's text
+        else: # Executes if there are no more instances of '[' on the current line
+            User_Story = User_Story + Line_Content # Builds the completed story
+            Line_Content = Outline.readline() # Moves on to the next line of 'Outline.txt'
+
+
+### Displays the User their customized MadLibs
+def Display_Story():
+
+    # Importing global variables
+    global User_Story
+
+    # Clearing the screen for readability
+    cls()
+
+    print(User_Story)
+
+### Exports the User's customized MadLibs into a new .txt file
+def Export_Story():
+
+    # Importing global variables
+    global User_Input
+    global User_Confirm
+    global User_Story
+
+    while User_Confirm == False: # Prevents the User from overwriting the 'Outline.txt' file
+        User_Input = input("Please enter what you would like to name the file that your story will be saved to: ")
+        if User_Input == "Outline":
+            print("*ERROR* You cannot overwrite the 'Outline.txt' file. *ERROR*")
+        else: # Stores the name of the new file once a valid entry has been entered
+            File_Name = User_Input + ".txt"
+            break
+
+    # Creating the custom .txt file
+    New_File = open(File_Name,"w")
+    New_File.write(User_Story) # Writing the User's story to the new file
+    print("Your custom story has been saved in a file called: " + File_Name)
+    New_File.close()
+    
+    input("\nPress 'enter' to continue.")
 
 ########################################################### PROGRAM FLOW #################################################################
 
@@ -45,7 +111,21 @@ while Is_Running == True:
     print("Welcome to Mad Libs!".center(80, " "))
     print("\nPlease enter the requested information to generate your custom Mad Lib.")
 
-    Load_File()
+    Load_File() # Loads the 'Outline.txt' file into the program
+    Prompt_User() # Prompts the User to enter information to customize the MadLibs
+    Display_Story() # Displays the User their customized MadLibs
+
+    while User_Confirm == False:
+        User_Input = input("\nWould you like to save your custom story to a new file? (y/n): ")
+        if User_Input.upper() == "Y":
+            Export_Story() # Exports the User's customized MadLibs into a new .txt file
+            break
+        elif User_Input.upper() == "N":
+            break
+        else:
+            print("Please enter a valid selection.")
+
+    Outline.close()
 
     while User_Confirm == False:
         User_Input = input("\nWould you like to play again? (y/n): ")
@@ -56,5 +136,3 @@ while Is_Running == True:
             break
         else:
             print("Please enter a valid selection.")
-
-    input("\nPress 'enter' to continue.")
--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This is a simple Mad Libs game based off of "Monty Python and the Quest for the Holy Grail."
 
 You will need to have the 'Outline.txt' file in the same folder as the 'MadLibs.py' file or the program will not work.
+
+This is a longer Mad Libs so don't worry if it seems like it is dragging on, it will pay off in the end.
+
+You will have the option to save your custom Mad Lib into a new .txt file if you would like to do so.
+
+To see more of my work please visit: https://github.com/thejacobhardman
+
+*This program was created for the Intro To Programming course at Aims Community College.*
